1d775dc2875e8cd1e90562764cda10a3beb0156f
Przemyslaw Idziaszek <przemyslaw.idziaszek@gmail.com>
2020-03-29 18:22:20 +0200

    Unit tests added

---
+++ Boar.Viewer.Tests/TestsApiBoarService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Api.BoarService.Controllers;
+using NUnit.Framework;
+
+namespace Boar.Viewer.Tests
+{
+
+    public class TestsApiBoarService
+    {
+        [Test]
+        public void Get_ShouldReturnAllBoars()
+        {
+            var controller = new  BoarController();
+
+            var result = controller.Get() as List<Api.BoarService.Models.Boar>;
+            Assert.AreEqual(3, result?.Count);
+        }
+    }
+}
---
+++ Boar.Viewer.Tests/TestsApiFoodService.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Api.FoodService.Controllers;
+using Api.FoodService.Models;
+using NUnit.Framework;
+
+namespace Boar.Viewer.Tests
+{
+    public class TestsApiFoodService
+    {
+        [Test]
+        public void Get_ShouldReturnAllFood()
+        {
+            var controller = new FoodController();
+
+            var result = controller.Get() as List<Food>;
+            Assert.AreEqual(4, result?.Count);
+        }
+    }
+}
